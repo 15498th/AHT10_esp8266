66e0cdfbf6285e0ce6d12accc011238383867fda
user <user@localhost>
2021-01-17 01:44:50 +0300

    Some optimizations: remove methods that seems unnecessary add properties to hold result values combine methods to read and convert in one

--- aht10.py
+++ aht10.py
@@ -13,8 +13,12 @@ class AHT10:
             raise ValueError('An I2C object is required.')
         self.i2c = i2c
         self.address = address
-        self.reset()
+        self.i2c.writeto(self.address, ResetCmd)
         self.raw_data = bytearray(6)
+        self.raw_temperature = 0
+        self.raw_humidity = 0
+        self.temperature = 0.0
+        self.humidity = 0.0
 
     def readStatus(self, from_buffer=False):
         if from_buffer:
@@ -26,57 +30,17 @@ class AHT10:
     def initiateMeasurement(self):
         self.i2c.writeto(self.address, MeasureCmd)
 
-    def reset(self):
-        self.i2c.writeto(self.address, ResetCmd)
-
-    def calibrate(self):
-        self.i2c.writeto(self.address, CalibrateCmd)
-
-    def readRawData(self):
+    def readData(self):
         self.raw_data = self.i2c.readfrom(self.address, 6)
-
-    def convertHumidity(self):
         raw_humidity = ((self.raw_data[1] << 16) | (self.raw_data[2] << 8) | self.raw_data[3]) >> 4
-        return raw_humidity * 100 / 1048576
+        self.humidity = raw_humidity * 100 / 1048576
 
-    def convertTemperature(self):
         raw_temperature = ((self.raw_data[3] & 0x0F) << 16) | (self.raw_data[4] << 8) | self.raw_data[5]
-        return ((200 * raw_temperature) / 1048576) - 50
-
-    def readAndConvert(self):
-        self.initiateMeasurement()
-        time.sleep_ms(100) 
-        self.readRawData()
-        hum = self.convertHumidity()
-        temp = self.convertTemperature()
-        return [hum, temp]
-
-    def statusCalibrated(self, from_buffer=False):
-        status = self.readStatus(from_buffer)
-        return self.bitIsSet(status,3)
-
-    def statusBusy(self, from_buffer=False):
-        status = self.readStatus(from_buffer)
-        return self.bitIsSet(status,7)
-
-    def statusMode(self, from_buffer=False):
-        status = self.readStatus(from_buffer)
-        if (self.bitIsSet(status,6)):
-            return 'CMD'
-        elif (self.bitIsSet(status,5)):
-            return 'CYC'
-        else:
-            return 'NOR'
-
-    def bitIsSet(self, byte, bit):
-        if (byte & (1<<bit) == 0 ):
-            return False
-        else:
-            return True
+        self.temperature = ((200 * raw_temperature) / 1048576) - 50
 
     @property
     def values(self):
-        """ human readable values """
-
-        h, t = self.readAndConvert()
-        return ("{:.2f}".format(t), "{:.2f}".format(h))
+        self.initiateMeasurement()
+        time.sleep_ms(100) 
+        self.readData()
+        return ("{:.2f}".format(self.temperature), "{:.2f}".format(self.humidity))
